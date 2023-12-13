88fe083dd4621c1fa9c82dbd5b2ae41544708fa9
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-12-13 14:18:35 -0600

    add @astro/sitemap + astro/icon, update fonts, basic typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://66thandbyrnes.com',
+  build: {
+    inlineStylesheets: 'always',
+  },
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
 })
