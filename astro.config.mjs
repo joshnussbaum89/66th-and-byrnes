3cafcd6134a691dadf5c53ec61cca2609a1ac313
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-12-17 17:15:25 -0600

    create Vision component for home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://66thandbyrnes.com',
+  scopedStyleStrategy: 'class',
   build: {
     inlineStylesheets: 'always',
   },
